7c9cde24f2ae9010352e10b15ed3bdfe9a8c9d27
g-kelvin <kelvingauki@gmail.com>
2020-06-09 23:21:57 +0300

    update

--- messaging.php
+++ messaging.php
@@ -104,7 +104,7 @@
 
 
 <?php 
-    if(!isset($_POST['submit'])){
+    if(!isset($_POST['submit'] )){
         $name = $_POST['name'];
         $email = $_POST['email'];
         $subject = $_POST['subject'];
